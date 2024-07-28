3b1a5198c694a33304ac6611696da66fb349a62c
brine-egg <BigBrine@proton.me>
2024-07-28 14:55:12 +1000

    Replace lualine.nvim and oil.nvim with mini.nvim modules

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -20,20 +20,31 @@ return {
       -- Add animations to common Neovim actions
       require('mini.animate').setup()
 
-      -- -- Simple and easy statusline.
-      -- --  You could remove this setup call if you don't like it,
-      -- --  and try some other statusline plugin
-      -- local statusline = require 'mini.statusline'
-      -- -- set use_icons to true if you have a Nerd Font
-      -- statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- Pop-up file manager
+      require('mini.files').setup {
+        windows = {
+          max_number = 5,
+          preview = true,
+          width_nofocus = 15,
+          width_focus = 30,
+          width_preview = 30,
+        },
+      }
 
-      -- -- You can configure sections in the statusline by overriding their
-      -- -- default behavior. For example, here we set the section for
-      -- -- cursor location to LINE:COLUMN
-      -- ---@diagnostic disable-next-line: duplicate-set-field
-      -- statusline.section_location = function()
-      --   return '%2l:%-2v'
-      -- end
+      -- Simple and easy statusline.
+      --  You could remove this setup call if you don't like it,
+      --  and try some other statusline plugin
+      local statusline = require 'mini.statusline'
+      -- set use_icons to true if you have a Nerd Font
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
